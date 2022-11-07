9ecc2454140397dfe768633a90b711b32b03eab2
Artemmakhlay <116097849+Artemmakhlay@users.noreply.github.com>
2022-11-07 15:55:28 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,40 @@
+let intensiter = 0
+function Bleu () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+function Rouge () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+}
+function blanc () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+function mauve () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+function vert () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+}
+basic.forever(function () {
+    intensiter = pins.analogReadPin(AnalogPin.P0)
+    if (intensiter >= 300) {
+        Bleu()
+    } else if (intensiter < 300 && intensiter >= 250) {
+        vert()
+    } else if (intensiter < 250 && intensiter >= 200) {
+        Rouge()
+    } else if (intensiter < 200 && intensiter >= 150) {
+        mauve()
+    } else {
+        blanc()
+    }
+})
